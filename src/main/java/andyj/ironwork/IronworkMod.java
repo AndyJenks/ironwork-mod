7b5a2adb7b447b7631233ff19ee7f8c86320371b
Andrew Jenkins <andjdev@gmail.com>
2022-09-01 17:41:11 +0100

    Add corrugated panels, blocks, slabs and stairs.

--- src/main/java/andyj/ironwork/IronworkMod.java
+++ src/main/java/andyj/ironwork/IronworkMod.java
@@ -9,6 +9,11 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.core.Registry;
+import net.minecraft.world.level.block.IronBarsBlock;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class IronworkMod implements ModInitializer {
@@ -21,7 +26,22 @@ public class IronworkMod implements ModInitializer {
 	public static final Block WARREN_TRUSS = new TrussBlock();
 	public static final Block CROSS_TRUSS = new TrussBlock();
 	public static final Block CROSS_TRUSS_PAINTED = new TrussBlock();
+	public static final Block CORRUGATED_PANEL = new IronBarsBlock(BlockBehaviour.Properties.of(Material.METAL));
+	public static final Block CORRUGATED_BLOCK = new Block(BlockBehaviour.Properties
+			.of(Material.METAL).strength(1.0f));
+	public static final StairBlock CORRUGATED_STAIRS = new StairBlock(CORRUGATED_BLOCK.defaultBlockState(),
+			BlockBehaviour.Properties.copy(CORRUGATED_BLOCK));
+	public static final SlabBlock CORRUGATED_SLAB = new SlabBlock(BlockBehaviour.Properties.copy(CORRUGATED_BLOCK));
 
+	private void registerBlockAndItem(String name, Block block, CreativeModeTab tab) {
+		Registry.register(Registry.BLOCK, new ResourceLocation(MODID, name), block);
+		Registry.register(Registry.ITEM, new ResourceLocation(MODID, name),
+				new BlockItem(block, new FabricItemSettings().group(tab)));
+	}
+
+	private void registerItem(String name, Item item) {
+		Registry.register(Registry.ITEM, new ResourceLocation(MODID, name), item);
+	}
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -30,15 +50,16 @@ public class IronworkMod implements ModInitializer {
 
 		LOGGER.info("Starting Ironwork mod");
 		LOGGER.info("Registering items");
-		Registry.register(Registry.ITEM, new ResourceLocation(MODID, "truss_element"), TRUSS_ELEMENT);
-		Registry.register(Registry.BLOCK, new ResourceLocation(MODID, "cross_truss_painted"), CROSS_TRUSS_PAINTED);
-		Registry.register(Registry.ITEM, new ResourceLocation(MODID, "cross_truss_painted"),
-				new BlockItem(CROSS_TRUSS_PAINTED, new FabricItemSettings().group(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-		Registry.register(Registry.BLOCK, new ResourceLocation(MODID, "cross_truss"), CROSS_TRUSS);
-		Registry.register(Registry.ITEM, new ResourceLocation(MODID, "cross_truss"),
-				new BlockItem(CROSS_TRUSS, new FabricItemSettings().group(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-		Registry.register(Registry.BLOCK, new ResourceLocation(MODID, "warren_truss"), WARREN_TRUSS);
-		Registry.register(Registry.ITEM, new ResourceLocation(MODID, "warren_truss"),
-				new BlockItem(WARREN_TRUSS, new FabricItemSettings().group(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+		registerItem("truss_element", TRUSS_ELEMENT);
+		registerBlockAndItem("cross_truss", CROSS_TRUSS, CreativeModeTab.TAB_BUILDING_BLOCKS);
+		registerBlockAndItem("cross_truss_painted", CROSS_TRUSS_PAINTED, CreativeModeTab.TAB_BUILDING_BLOCKS);
+		registerBlockAndItem("warren_truss", WARREN_TRUSS, CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+		// Corrugated bits
+		registerBlockAndItem("corrugated_panel", CORRUGATED_PANEL, CreativeModeTab.TAB_DECORATIONS);
+		registerBlockAndItem("corrugated_panel_block", CORRUGATED_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+		registerBlockAndItem("corrugated_stairs", CORRUGATED_STAIRS, CreativeModeTab.TAB_BUILDING_BLOCKS);
+		registerBlockAndItem("corrugated_slab", CORRUGATED_SLAB, CreativeModeTab.TAB_BUILDING_BLOCKS);
 	}
 }
